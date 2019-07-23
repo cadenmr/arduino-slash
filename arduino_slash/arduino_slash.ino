652906739163c1120d4fc433defd17db94efdff5
cadenmr <37202505+cadenmr@users.noreply.github.com>
2019-07-22 20:51:03 -0700

    Pass through the reciever's servo

--- arduino_slash/arduino_slash.ino
+++ arduino_slash/arduino_slash.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+// Provides computer control for the 1/16 Slash VXL 4x4 RC car
+// Arduino is intended to be placed in between the reciever and the ESC and servo
 
+#include <Servo.h>
+
+// PIN ASSIGNMENTS
+int THRO_INPUT = 3;
+int STEER_INPUT = 5;
+int THRO_OUTPUT = 6;
+int STEER_OUTPUT = 9;
+
+// DEFINE VARIABLES
+int THRO_POSITION = 0;
+int SERIAL_DATA;
+int SERIAL_CHAR;
+
+// DEFINE OBJECTS
+Servo STEER_SERVO;
+
+void setup() {
+  pinMode(THRO_INPUT, INPUT);
+  pinMode(THRO_OUTPUT, OUTPUT);
+  pinMode(STEER_INPUT, INPUT);
+  STEER_SERVO.attach(STEER_OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  THRO_POSITION = pulseIn(THRO_INPUT, HIGH);
+  
+  Serial.println(map(THRO_POSITION, 970, 1960, 0, 180));
+  
+  STEER_SERVO.write(map(THRO_POSITION, 970, 1960, 0, 180));
 }
