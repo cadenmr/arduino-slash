e3cf3b59dd795c6eb6b8b1c017fd2a7158c90fa0
cadenmr <37202505+cadenmr@users.noreply.github.com>
2019-07-23 22:35:49 -0700

    Overhaul and switch to interrupts for pwm input handling
    closes #2 as not neccesary

--- arduino_slash/arduino_slash.ino
+++ arduino_slash/arduino_slash.ino
@@ -4,34 +4,57 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
 
-#define THRO_INPUT 2
-#define STEER_INPUT 4
-#define THRO_OUTPUT 3
+#define STEER_INPUT 2
+#define THRO_INPUT 3
+#define THRO_OUTPUT 4
 #define STEER_OUTPUT 5
 
-// DEFINE VARIABLES
-int THRO_POSITION = 0;
-int STEER_POSITION = 0;
+volatile int pwm_value_1 = 0;
+volatile int prev_time_1 = 0;
+volatile int pwm_value_2 = 0;
+volatile int prev_time_2 = 0;
 
 // DEFINE OBJECTS
 Servo STEER_SERVO;
+Servo ESC;
 SoftwareSerial Bluetooth(10, 9);
 
 void setup() {
-  pinMode(THRO_INPUT, INPUT);
-  pinMode(THRO_OUTPUT, OUTPUT);
-  pinMode(STEER_INPUT, INPUT);
+  //pinMode(THRO_OUTPUT, OUTPUT);
   STEER_SERVO.attach(STEER_OUTPUT);
+  ESC.attach(THRO_OUTPUT);
 
   Serial.begin(9600);
   Bluetooth.begin(9600);
+
+  attachInterrupt(0, rising_1, RISING);
+  attachInterrupt(1, rising_2, RISING);
 }
 
 void loop() {
-  STEER_POSITION = pulseIn(STEER_INPUT, HIGH);
-  //Serial.println(map(STEER_POSITION, 970, 1960, 0, 180));
-  STEER_SERVO.write(map(STEER_POSITION, 970, 1960, 0, 180));
+  STEER_SERVO.write(pwm_value_1);
+  //Serial.println("Steer: " + String(pwm_value_1));
 
-  THRO_POSITION = pulseIn(THRO_INPUT, HIGH);
-  Serial.println(THRO_POSITION);
+  //Serial.println("Throttle: " + String(pwm_value_2));
+  ESC.write(pwm_value_2);
+}
+
+void rising_1() {
+  attachInterrupt(0, falling_1, FALLING);
+  prev_time_1 = micros();
+}
+ 
+void falling_1() {
+  attachInterrupt(0, rising_1, RISING);
+  pwm_value_1 = micros()-prev_time_1;
+}
+
+void rising_2() {
+  attachInterrupt(1, falling_2, FALLING);
+  prev_time_2 = micros();
+}
+ 
+void falling_2() {
+  attachInterrupt(1, rising_2, RISING);
+  pwm_value_2 = micros()-prev_time_2;
 }
