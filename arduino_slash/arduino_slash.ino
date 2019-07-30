b4c20569f82777c1125179ef5407f5ac741ecf8b
cadenmr <37202505+cadenmr@users.noreply.github.com>
2019-07-30 03:18:55 -0700

    Major Overhaul

--- arduino_slash/arduino_slash.ino
+++ arduino_slash/arduino_slash.ino
@@ -14,24 +14,60 @@ volatile int prev_time_1 = 0;
 volatile int pwm_value_2 = 0;
 volatile int prev_time_2 = 0;
 
+<<<<<<< Updated upstream
 // DEFINE OBJECTS
+=======
+char steer_state_byte = '+';
+char thro_state_byte = '+';
+
+char command_byte_steer_ones = '0';
+char command_byte_steer_tens = '0';
+char command_byte_steer_hunds = '0';
+char command_byte_steer_thous = '0';
+
+char command_byte_thro_ones = '0';
+char command_byte_thro_tens = '0';
+char command_byte_thro_hunds = '0';
+char command_byte_thro_thous = '0';
+
+int steer_command;
+int thro_command;
+
+String recieved_data;
+
+>>>>>>> Stashed changes
 Servo STEER_SERVO;
 Servo ESC;
-SoftwareSerial Bluetooth(10, 9);
+SoftwareSerial Bluetooth(11, 10); // RX, TX
 
 void setup() {
   //pinMode(THRO_OUTPUT, OUTPUT);
   STEER_SERVO.attach(STEER_OUTPUT);
   ESC.attach(THRO_OUTPUT);
 
+<<<<<<< Updated upstream
   Serial.begin(9600);
   Bluetooth.begin(9600);
+=======
+  STEER_SERVO.write(90);
+  //ESC.write(90);
 
-  attachInterrupt(0, rising_1, RISING);
-  attachInterrupt(1, rising_2, RISING);
+  Serial.begin(115200);
+  Serial.setTimeout(2);
+  Bluetooth.begin(57600);
+  Bluetooth.setTimeout(7);
+>>>>>>> Stashed changes
+
+  Bluetooth.println("Ready");
+
+  //Serial.println("Ready");
+
+  //attachInterrupt(0, rising_1, RISING);
+  //attachInterrupt(1, rising_2, RISING);
 }
 
 void loop() {
+<<<<<<< Updated upstream
   STEER_SERVO.write(pwm_value_1);
   //Serial.println("Steer: " + String(pwm_value_1));
 
@@ -41,6 +77,15 @@ void loop() {
   } else {
     ESC.write(pwm_value_2);
   }
+=======
+  read_serial_data();
+  
+  //STEER_SERVO.write(pwm_value_1);
+  STEER_SERVO.write(map(steer_command, -2000, 2000, 0, 180));
+  ESC.write(map(thro_command, -1000, 1000, 950, 1950));
+
+  //delay(2);
+>>>>>>> Stashed changes
 }
 
 void rising_1() {
@@ -62,3 +107,53 @@ void falling_2() {
   attachInterrupt(1, rising_2, RISING);
   pwm_value_2 = micros()-prev_time_2;
 }
+<<<<<<< Updated upstream
+=======
+
+// Serial data input in accordance to data struct
+// +-0000,+-0000
+//  steer,throttle
+void read_serial_data() {
+  if (Serial.available()) {
+    recieved_data = Serial.readString();
+
+    thro_state_byte = char(recieved_data[6]);
+    steer_state_byte = char(recieved_data[0]);
+    
+    command_byte_steer_ones = char(recieved_data[1]);
+    command_byte_steer_tens = char(recieved_data[2]);
+    command_byte_steer_hunds = char(recieved_data[3]);
+    command_byte_steer_thous = char(recieved_data[4]);
+
+    steer_command = ((command_byte_steer_ones - '0') * 1000) + ((command_byte_steer_tens - '0') * 100) + ((command_byte_steer_hunds - '0') * 10) + (command_byte_steer_thous - '0');
+
+    if (steer_state_byte == '-') {
+      steer_command = steer_command - (steer_command * 2);
+    }
+
+    command_byte_thro_ones = char(recieved_data[7]);
+    command_byte_thro_tens = char(recieved_data[8]);
+    command_byte_thro_hunds = char(recieved_data[9]);
+    command_byte_thro_thous = char(recieved_data[10]);
+
+    thro_command = ((command_byte_thro_ones - '0') * 1000) + ((command_byte_thro_tens - '0') * 100) + ((command_byte_thro_hunds - '0') * 10) + (command_byte_thro_thous - '0');
+
+    if (thro_state_byte == '-') {
+      thro_command = thro_command - (thro_command * 2);
+    }
+
+    
+    recieved_data = "";
+    //Serial.flush();
+  }
+}
+
+// esc speed setter shortcut with deadzone
+void esc_set_speed(int value) {
+  if (value > 1950) {
+    ESC.write(2000);
+  } else {
+    ESC.write(value);
+  }
+}
+>>>>>>> Stashed changes
